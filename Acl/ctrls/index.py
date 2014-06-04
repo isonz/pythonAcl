d0b78bb99361ca97ef2ac7bd71f21750ee66556c
ison-hp <ison@hp.com>
2014-06-05 00:30:34 +0800

    some one

--- Acl/ctrls/index.py
+++ Acl/ctrls/index.py
@@ -1,10 +1,14 @@
 from CBase import CBase
 
 class Index(CBase):
+    parent = None
+    
     def __init__(self):
-        pass
+        self.parent = super(Index, self)
+        self.parent.__init__()
+        
     def GET(self):
-       return super(Index, self).auth()
+       return self.parent.auth()
 #
 #       todos1 = db.select(tb, order='finished asc, id asc')
 #       todos2 = db.select(tb, order='finished asc, id asc')
